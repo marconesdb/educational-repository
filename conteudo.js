31eb84ce2cf6afe2aef8dfc14590eeb73600cf1d
marconebrito <marcone.brito@elystech.com.br>
2024-06-15 21:52:52 -0300

    Alteraçao na responsividade e nos cards

--- conteudo.js
+++ conteudo.js
@@ -1,95 +1,96 @@
 // Aguarda o carregamento completo do DOM
 document.addEventListener('DOMContentLoaded', function () {
-  // Referências aos elementos da página
-  const contentList = document.getElementById('content-list');
-  const loadingSpinner = document.getElementById('loadingSpinner');
-  const urlParams = new URLSearchParams(window.location.search);
-  const query = urlParams.get('query') ? urlParams.get('query').toLowerCase() : '';
-
-  // Exibe mensagem se não houver consulta de pesquisa
-  if (!query) {
-      contentList.innerHTML = '<p class="text-center text-gray-600">Nenhum resultado encontrado.</p>';
-      loadingSpinner.style.display = 'none';
-      return;
-  }
-
-  // Obter os resultados da pesquisa do arquivo JSON
-  fetch('conteudos.json')
-      .then(response => response.json())
-      .then(contents => {
-          // Filtra os conteúdos com base na consulta de pesquisa
-          const filteredContents = contents.filter(content =>
-              content.title.toLowerCase().includes(query) ||
-              content.type.toLowerCase().includes(query) ||
-              content.description.toLowerCase().includes(query) ||
-              content.category.toLowerCase().includes(query)
-          );
-
-          // Esconde o spinner de carregamento
-          loadingSpinner.style.display = 'none';
-
-          // Exibe mensagem se nenhum resultado for encontrado
-          if (filteredContents.length === 0) {
-              contentList.innerHTML = '<p class="text-center text-gray-600">Nenhum resultado encontrado.</p>';
-              return;
-          }
-
-          // Exibir os resultados na página
-          filteredContents.forEach(content => {
-              const contentItem = document.createElement('div');
-
-              // Aumenta a largura do card em 1%
-              contentItem.className = 'bg-white p-4 rounded shadow content-item w-full sm:w-96';
-
-              let contentHtml = '';
-
-              // Exibir conteúdo baseado no tipo
-              switch (content.type) {
-                  case 'video':
-                      // Verifica se é um link do YouTube
-                      if (content.src.includes('youtube.com')) {
-                          const videoId = getYoutubeVideoId(content.src);
-                          contentHtml = `<div class="relative" style="padding-bottom: calc(56.25% + 10%);">
-                          <iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen class="absolute top-0 left-0 w-full h-full"></iframe>
-                          </div>
-                            `;
-                        } 
+    // Referências aos elementos da página
+    const contentList = document.getElementById('content-list');
+    const loadingSpinner = document.getElementById('loadingSpinner');
+    const urlParams = new URLSearchParams(window.location.search);
+    const query = urlParams.get('query') ? urlParams.get('query').toLowerCase() : '';
+  
+    // Exibe mensagem se não houver consulta de pesquisa
+    if (!query) {
+        contentList.innerHTML = '<p class="text-center text-gray-600">Nenhum resultado encontrado.</p>';
+        loadingSpinner.style.display = 'none';
+        return;
+    }
+  
+    // Obter os resultados da pesquisa do arquivo JSON
+    fetch('conteudos.json')
+        .then(response => response.json())
+        .then(contents => {
+            // Filtra os conteúdos com base na consulta de pesquisa
+            const filteredContents = contents.filter(content =>
+                content.title.toLowerCase().includes(query) ||
+                content.type.toLowerCase().includes(query) ||
+                content.description.toLowerCase().includes(query) ||
+                content.category.toLowerCase().includes(query)
+            );
+  
+            // Esconde o spinner de carregamento
+            loadingSpinner.style.display = 'none';
+  
+            // Exibe mensagem se nenhum resultado for encontrado
+            if (filteredContents.length === 0) {
+                contentList.innerHTML = '<p class="text-center text-gray-600">Nenhum resultado encontrado.</p>';
+                return;
+            }
+  
+            // Exibir os resultados na página
+            filteredContents.forEach(content => {
+                const contentItem = document.createElement('div');
+  
+                // Aumenta a largura do card em 1%
+                contentItem.className = 'bg-white p-4 rounded shadow content-item w-full sm:w-96 md:w-80';
+  
+                let contentHtml = '';
+  
+                // Exibir conteúdo baseado no tipo
+                switch (content.type) {
+                    case 'video':
+                        // Verifica se é um link do YouTube
+                        if (content.src.includes('youtube.com')) {
+                            const videoId = getYoutubeVideoId(content.src);
+                            contentHtml = `<div class="relative" style="padding-bottom: calc(56.25% + 10%);">
+                            <iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen class="absolute top-0 left-0 w-full h-full"></iframe>
+                            </div>
+                              `;
+                          } 
+                          break;
+                    case 'pdf':
+                        contentHtml = `
+                            <img src="${content.thumbnail || './img/icone-pdf.png'}" alt="${content.title}" class="w-full fixed-height object-cover">
+                            <a href="${content.src}" download="${content.title}.pdf" class="block bg-blue-500 text-white px-4 py-2 rounded mt-2 text-center">Download</a>
+                        `;
                         break;
-                  case 'pdf':
-                      contentHtml = `
-                          <img src="${content.thumbnail || './img/icone-pdf.png'}" alt="${content.title}" class="w-full fixed-height object-cover">
-                          <a href="${content.src}" download="${content.title}.pdf" class="block bg-blue-500 text-white px-4 py-2 rounded mt-2 text-center">Download</a>
-                      `;
-                      break;
-                  case 'apresentacao':
-                      contentHtml = `
-                          <img src="${content.thumbnail || './img/icone-pptx.png'}" alt="${content.title}" class="w-full fixed-height object-cover">
-                          <a href="${content.src}" download="${content.title}.pptx" class="block bg-blue-500 text-white px-4 py-2 rounded mt-2 text-center">Download</a>
-                      `;
-                      break;
-              }
-
-              // Adiciona o conteúdo ao item da lista
-              contentItem.innerHTML = `
-                  ${contentHtml}
-                  <h2 class="text-xl font-bold mt-4">${content.title}</h2>
-                  <p>${content.description}</p>
-              `;
-
-              // Adiciona o item à lista de conteúdos
-              contentList.appendChild(contentItem);
-          });
-      })
-      .catch(error => {
-          console.error('Erro ao obter os resultados da pesquisa:', error);
-          loadingSpinner.style.display = 'none';
-          contentList.innerHTML = '<p class="text-center text-gray-600">Erro ao carregar os conteúdos.</p>';
-      });
-
-  // Função para obter o ID do vídeo do YouTube a partir do link
-  function getYoutubeVideoId(url) {
-      const regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
-      const match = url.match(regExp);
-      return (match && match[2].length === 11) ? match[2] : null;
-  }
-});
+                    case 'apresentacao':
+                        contentHtml = `
+                            <img src="${content.thumbnail || './img/icone-pptx.png'}" alt="${content.title}" class="w-full fixed-height object-cover">
+                            <a href="${content.src}" download="${content.title}.pptx" class="block bg-blue-500 text-white px-4 py-2 rounded mt-2 text-center">Download</a>
+                        `;
+                        break;
+                }
+  
+                // Adiciona o conteúdo ao item da lista
+                contentItem.innerHTML = `
+                    ${contentHtml}
+                    <h2 class="text-xl font-bold mt-4">${content.title}</h2>
+                    <p>${content.description}</p>
+                `;
+  
+                // Adiciona o item à lista de conteúdos
+                contentList.appendChild(contentItem);
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao obter os resultados da pesquisa:', error);
+            loadingSpinner.style.display = 'none';
+            contentList.innerHTML = '<p class="text-center text-gray-600">Erro ao carregar os conteúdos.</p>';
+        });
+  
+    // Função para obter o ID do vídeo do YouTube a partir do link
+    function getYoutubeVideoId(url) {
+        const regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
+        const match = url.match(regExp);
+        return (match && match[2].length === 11) ? match[2] : null;
+    }
+  });
+  
--- iniciante/iniciante.js
+++ iniciante/iniciante.js
@@ -1,83 +1,79 @@
-// Aguarda o carregamento completo do DOM
 document.addEventListener('DOMContentLoaded', function () {
-  const contentGrid = document.getElementById('content-grid');
+    const contentGrid = document.getElementById('content-grid');
 
-  // URL do arquivo conteudos.json
-  const conteudosJsonUrl = '../conteudos.json';
+    // URL do arquivo conteudos.json
+    const conteudosJsonUrl = '../conteudos.json';
 
-  // Função para carregar e exibir o conteúdo do JSON
-  function carregarConteudos() {
-      fetch(conteudosJsonUrl)
-          .then(response => {
-              if (!response.ok) {
-                  throw new Error('Erro ao carregar o arquivo JSON');
-              }
-              return response.json();
-          })
-          .then(results => {
-              // Filtrar apenas os conteúdos da categoria 'iniciante'
-              const conteudosIniciantes = results.filter(content => content.category === 'iniciante');
+    // Função para carregar e exibir o conteúdo do JSON
+    function carregarConteudos() {
+        fetch(conteudosJsonUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao carregar o arquivo JSON');
+                }
+                return response.json();
+            })
+            .then(results => {
+                // Filtrar apenas os conteúdos da categoria 'iniciante'
+                const conteudosIniciantes = results.filter(content => content.category === 'iniciante');
 
-              // Limpar conteúdo existente, se houver
-              contentGrid.innerHTML = '';
+                // Limpar conteúdo existente, se houver
+                contentGrid.innerHTML = '';
 
-              // Iterar sobre os resultados filtrados e criar elementos HTML correspondentes
-              conteudosIniciantes.forEach(content => {
-                  const contentItem = document.createElement('div');
-                 
+                // Iterar sobre os resultados filtrados e criar elementos HTML correspondentes
+                conteudosIniciantes.forEach(content => {
+                    const contentItem = document.createElement('div');
+                    contentItem.className = 'content-item';
 
-                  // Aumenta a largura do card em 1%
-                  contentItem.className = 'bg-white p-4 rounded shadow content-item w-full sm:w-96';
+                    let contentHtml = '';
 
-                  let contentHtml = '';
+                    // Verifica o tipo de conteúdo e gera o HTML correspondente
+                    switch (content.type) {
+                        case 'video':
+                            const videoId = getYoutubeVideoId(content.src);
+                            contentHtml = `
+                                <div class="relative" style="padding-bottom: calc(56.25% + 10%);">
+                                    <iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen class="absolute top-0 left-0 w-full h-full"></iframe>
+                                </div>
+                            `;
+                            break;
+                        case 'pdf':
+                            contentHtml = `
+                                <img src="../img/icone-pdf.png" alt="${content.title}" class="w-full h-auto object-cover">
+                                <a href="${content.src}" download="${content.title}.pdf" class="block bg-blue-500 text-white px-4 py-2 rounded mt-2 text-center">Download</a>
+                            `;
+                            break;
+                        case 'apresentacao':
+                            contentHtml = `
+                                <img src="../img/icone-pptx.png" alt="${content.title}" class="w-full h-auto object-cover">
+                                <a href="${content.src}" download="${content.title}.pptx" class="block bg-blue-500 text-white px-4 py-2 rounded mt-2 text-center">Download</a>
+                            `;
+                            break;
+                    }
 
-                  // Verifica o tipo de conteúdo e gera o HTML correspondente
-                  switch (content.type) {
-                      case 'video':
-                          const videoId = getYoutubeVideoId(content.src);
-                          contentHtml = `
-                          <div class="relative" style="padding-bottom: calc(56.25% + 10%);">
-                          <iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen class="absolute top-0 left-0 w-full h-full"></iframe>
-                          </div>
-                          `;
-                          break;
-                      case 'pdf':
-                          contentHtml = `
-                              <img src="${ '../img/icone-pdf.png'}" alt="${content.title}" class="w-full h-auto object-cover">
-                              <a href="${content.src}" download="${content.title}.pdf" class="block bg-blue-500 text-white px-4 py-2 rounded mt-2 text-center">Download</a>
-                          `;
-                          break;
-                      case 'apresentacao':
-                          contentHtml = `
-                              <img src="${'../img/icone-pptx.png'}" alt="${content.title}" class="w-full h-auto  object-cover">
-                              <a href="${content.src}" download="${content.title}.pptx" class="block bg-blue-500 text-white px-4 py-2 rounded mt-2 text-center">Download</a>
-                          `;
-                          break;
-                  }
+                    // Adiciona o conteúdo ao item da lista
+                    contentItem.innerHTML = `
+                        ${contentHtml}
+                        <h2 class="text-xl font-bold mt-4">${content.title}</h2>
+                        <p>${content.description}</p>
+                    `;
 
-                  // Adiciona o conteúdo ao item da lista
-                  contentItem.innerHTML = `
-                      ${contentHtml}
-                      <h2 class="text-xl font-bold mt-4">${content.title}</h2>
-                      <p>${content.description}</p>
-                  `;
+                    // Adiciona o item ao grid de conteúdos
+                    contentGrid.appendChild(contentItem);
+                });
+            })
+            .catch(error => {
+                console.error('Erro ao carregar conteúdos:', error);
+            });
+    }
 
-                  // Adiciona o item ao grid de conteúdos
-                  contentGrid.appendChild(contentItem);
-              });
-          })
-          .catch(error => {
-              console.error('Erro ao carregar conteúdos:', error);
-          });
-  }
+    // Função para obter o ID do vídeo do YouTube a partir do link
+    function getYoutubeVideoId(url) {
+        const regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
+        const match = url.match(regExp);
+        return match && match[2].length === 11 ? match[2] : null;
+    }
 
-  // Função para obter o ID do vídeo do YouTube a partir do link
-  function getYoutubeVideoId(url) {
-      const regExp = /^.*(youtu.be\/|v\/|e\/|u\/\\w+\/|embed\/|v=)([^#\\&\\?]*).*/;
-      const match = url.match(regExp);
-      return (match && match[2].length === 11) ? match[2] : null;
-  }
-
-  // Chamar a função para carregar os conteúdos quando a página é carregada
-  carregarConteudos();
+    // Chamar a função para carregar conteúdos ao carregar a página
+    carregarConteudos();
 });
